6c4d2fe67f280ab2a901080cb0dae4e0a4d9d70e
Muhammed Sameer <sysad@madinix.com>
2011-11-09 09:16:02 -0500

    Not deleting the localtime file, just incase

--- Utils/set_timezone_IST.sh
+++ Utils/set_timezone_IST.sh
@@ -2,9 +2,12 @@
 
 if [ -e /usr/share/zoneinfo/Asia/Calcutta ]
 then
-       rm -f /etc/localtime
-       ln -s /usr/share/zoneinfo/Asia/Calcutta /etc/localtime
-       ntpdate 0.us.pool.ntp.org
+        mv /etc/localtime /etc/localtime.orig
+	if ! ln -s /usr/share/zoneinfo/Asia/Calcutta /etc/localtime
+	then
+		mv /etc/localtime.orig /etc/localtime
+	fi
+        ntpdate 0.us.pool.ntp.org
 else
        echo "Cannot set timezone, as IST timezone does not exist"
        exit 1
